6e31614f3e6df7593c142f20793e7fa7873e8170
77web <info@77-web.com>
2011-12-26 17:52:16 +0900

    use JSMin.php

--- lib/util/Minify.class.php
+++ lib/util/Minify.class.php
@@ -18,37 +18,7 @@ class Minify
   
   public static function minifyJavascript($script)
   {
-    $params = array();
-    $params['js_code'] = $script;
-    $params['compilation_level'] = 'SIMPLE_OPTIMIZATIONS';
-    $params['output_format'] = 'text';
-    $params['output_info'] = 'compiled_code';
-    
-    $sock = @fsockopen('closure-compiler.appspot.com', 80, $errorno, $errorstr, 30);
-    if($sock)
-    {
-      $param = http_build_query($params);
-      
-      $post = array();
-      $post[] = 'POST /compile HTTP/1.1';
-      $post[] = 'Host: closure-compiler.appspot.com';
-      $post[] = 'Content-Length: '.strlen($param);
-      $post[] = 'Content-Type: application/x-www-form-urlencoded';
-      $post[] = 'Connection: close';
-      $post[] = '';
-      $post[] = $param;
-      
-      fputs($sock, implode("\r\n", $post));
-      $response = '';
-      while(!feof($sock))
-      {
-        $response .= fgets($sock);
-      }
-      fclose($sock);
-      $res = explode("\r\n\r\n", $response);
-      $script = $res[1];
-    }
-    
-    return $script;
+    require_once dirname(dirname(__FILE__)).'/vendor/jsmin-php/jsmin.php';
+    return JSMin::minify($script);
   }
 }
